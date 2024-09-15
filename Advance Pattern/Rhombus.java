636aa5ebb70b3eff366a302288f2e66ee2dd9d30
faij1207 <faij12072000@gmail.com>
2024-09-15 18:52:13 +0530

    rhombus pattern

---
+++ Advance Pattern/Rhombus.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Rhombus {
+    public static void pattern(int n){
+        for(int i=1; i<=n; i++){ //outer
+
+            //space
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            //star
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter you row size :");
+        int num = sc.nextInt();
+        pattern(num);
+    }
+}
+
+
+// output
+
+// enter you row size :
+// 5
+//     *****
+//    *****
+//   *****
+//  *****
+// *****
