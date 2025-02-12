3db33f8aab5a1fd5fe8b1e948f4d84ec46c3d858
faij1207 <faij12072000@gmail.com>
2025-02-12 19:33:19 +0530

    Add CountSetBits class to count the number of set bits in an integer

---
+++ 07-Bit-Manipulation/CountSetBits.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class CountSetBits {
+
+    public static int countSetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+           if((n & 1) != 0) { //check our LSB
+               count++;
+           }
+           n=n>>1; //right shift
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.println(countSetBits(n));
+    }   
+}
