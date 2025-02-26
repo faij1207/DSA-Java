9f5053e66fcf31ab6ac04e83f12383099502bc7b
faij1207 <faij12072000@gmail.com>
2025-02-26 11:34:18 +0530

    Add optimized power calculation method using recursion

--- 09-Recursion/Power.java
+++ 09-Recursion/Power.java
@@ -14,10 +14,27 @@ public class Power {
         return x * power(x, n - 1);
     }
 
+    //optimized solution
+    //time complexity: O(log n)
+    public static int powerOptimized(int x, int n) {
+        //base case
+        if (n == 0) {
+            return 1;
+        }
+        //recursive case
+        int temp = powerOptimized(x, n / 2);
+        if (n % 2 == 0) {
+            return temp * temp;
+        } else {
+            return x * temp * temp;
+        }
+    }
+
     public static void main(String[] args) {
         int x = 2;
         int n = 3;
         System.out.println(x + " raised to the power " + n + " is: " + power(x, n));
+        System.out.println(x + " raised to the power " + n + " is: " + powerOptimized(x, n));
     }
     
 }
