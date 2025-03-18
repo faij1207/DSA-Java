9ba4794eb8c6f6a656180b3e75510d9d7dc39ecd
faij1207 <faij12072000@gmail.com>
2025-03-18 16:05:42 +0530

    Refactor storeWater method in ContainerMostWater class to implement an optimized two-pointer approach for calculating maximum water storage

--- 12-ArrayList/ContainerMostWater.java
+++ 12-ArrayList/ContainerMostWater.java
@@ -4,20 +4,45 @@ public class ContainerMostWater {
 
     //Brute force approach
     //Time complexity: O(n^2)
+    // public static int storeWater(ArrayList<Integer> height){
+    //     int maxWater = 0;
+    //     for(int i = 0; i < height.size(); i++){
+    //         for(int j = i + 1; j < height.size(); j++){
+    //             int ht= Math.min(height.get(i), height.get(j));
+    //             int wd = j - i;
+    //             int water = ht * wd;
+    //             maxWater = Math.max(maxWater, water);
+    //         }
+    //     }
+    //     return maxWater;  
+    // }
+
+    
+    //Optimized approach (Two pointer approach)
+    //Time complexity: O(n)
+
     public static int storeWater(ArrayList<Integer> height){
         int maxWater = 0;
-        for(int i = 0; i < height.size(); i++){
-            for(int j = i + 1; j < height.size(); j++){
-                int ht= Math.min(height.get(i), height.get(j));
-                int wd = j - i;
-                int water = ht * wd;
-                maxWater = Math.max(maxWater, water);
+        int leftPointer = 0;
+        int rightPointer = height.size() - 1;
+        while(leftPointer < rightPointer){
+
+            //calculate the water stored between the two containers
+            int ht = Math.min(height.get(leftPointer), height.get(rightPointer));
+            int wd = rightPointer - leftPointer;
+            int water = ht * wd;
+            maxWater = Math.max(maxWater, water);
+
+            //update the pointers
+            //if the height of the left container is less than the height of the right container, move the left pointer to the right
+            if(height.get(leftPointer) < height.get(rightPointer)){
+                leftPointer++;
+            }else{
+                rightPointer--;
             }
         }
-        return maxWater;  
+        return maxWater;
     }
-
-
     
     public static void main(String[] args) {
         // Create an ArrayList of Integers
