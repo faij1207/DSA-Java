1d05bfed9a9a278eda4631aa040ea532bd479331
faij1207 <faij12072000@gmail.com>
2025-03-22 22:44:38 +0530

    Add functionality to insert nodes at the end of the linked list and print the list

--- 13-Linked-List/Insertion.java
+++ 13-Linked-List/Insertion.java
@@ -35,12 +35,53 @@ public class  Insertion{
         head = newNode;
     }
 
+    // Function to add a new node at the end of the linked list
+    // Time Complexity: O(1)
+    public void addNodeAtEnd(int data) {
+        //step 1: create a new node
+        Node newNode = new Node(data);
+
+        //check if linked list is empty
+        if (head == null) {
+           head= tail = newNode;  //if linked list is empty, then new node is both head and tail
+           return;
+        }
+
+        //step 2: point tail to new node
+        tail.next = newNode; //linking tail to new node
+
+        //step 3: make new node as tail
+        tail = newNode;
+    }
+
+    //print linked list
+    public void printList() {
+        //check if linked list is empty
+        if (head == null) {
+            System.out.println("Linked list is empty");
+            return;       
+        }
+        Node temp = head;
+        //traverse the linked list and print data
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
         Insertion ll=new Insertion();
-        ll.head = new Node(1);
 
         // Adding new node at start of the linked list
-        ll.addNodeAtStart(2);
         ll.addNodeAtStart(3);
+        ll.addNodeAtStart(2);
+
+        // Adding new node at end of the linked list
+        ll.addNodeAtEnd(4);
+        ll.addNodeAtEnd(5);
+
+        // Printing the linked list
+        ll.printList();
     }
 }
